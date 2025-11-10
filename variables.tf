69b53253261494d37ebc4663270d803907509df2
swaroop1032 <161610017+swaroop1032@users.noreply.github.com>
2025-11-10 15:25:35 +0530

    Refactor variable descriptions in variables.tf
    Removed comments and simplified descriptions for variables.

--- variables.tf
+++ variables.tf
@@ -1,24 +1,24 @@
 variable "pm_api_url" {
-  description = "The URL for the Proxmox API (e.g., https://192.168.1.10:8006)"
+  description = "The URL for the Proxmox API"
   type        = string
 }
 
 variable "pm_api_token_id" {
   description = "The Proxmox API Token ID"
   type        = string
-  sensitive   = true # Marks this as a secret
+  sensitive   = true
 }
 
 variable "pm_api_token_secret" {
   description = "The Proxmox API Token Secret"
   type        = string
-  sensitive   = true # Marks this as a secret
+  sensitive   = true
 }
 
 variable "proxmox_node" {
   description = "The name of the Proxmox node to deploy to"
   type        = string
-  default     = "pve" # Change this to your node name
+  default     = "pve"
 }
 
 variable "vm_template_name" {
@@ -26,9 +26,3 @@ variable "vm_template_name" {
   type        = string
   default     = "ubuntu-2204-cloudinit-template" # Change this to your template name
 }
-
-# variable "ssh_public_key" {
-#   description = "Public SSH key for the cloud-init user"
-#   type        = string
-#   default     = "ssh-rsa AAAAB3NzaC1yc2EAAA..." # Paste your public key here
-# }
